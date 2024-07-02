176d4173b115e2728b8da0246e5601e1ebb218ca
GeraldoRiberia <91908767+GeraldoRiberia@users.noreply.github.com>
2024-07-02 22:57:10 +0530

    homework
    -counter

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import { Button, Typography } from "@mui/material";
+import { useState } from "react";
+function App() {
+  var [count, setCount] = useState(0);
+  return (
+    <>
+      <div>
+        <Typography id="header" variant="h1">
+          Counter
+        </Typography>
+        <br />
+        <br />
+        <Typography id="counter" variant="h2">
+          {count}
+        </Typography>
+        <br />
+        <br />
+        <Button
+          onClick={() => {
+            setCount(--count);
+          }}
+          id="btminus"
+          variant="contained"
+        >
+          _
+        </Button>
+        <Button
+          onClick={() => {
+            setCount(++count);
+          }}
+          id="btplus"
+          variant="contained"
+        >
+          +
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
